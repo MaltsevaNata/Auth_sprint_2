b78064a085dd1f9f1f733a45640c73f0507f5007
Natalia <maltseva@arrival.com>
2021-07-25 16:37:34 +0300

    added response schemas

--- auth_service/src/api/v1/utils/schemas.py
+++ auth_service/src/api/v1/utils/schemas.py
@@ -15,6 +15,11 @@ class SignOutSchema(ma.Schema):
     refresh_token = fields.String(required=True)
 
 
+class RefreshSchema(ma.Schema):
+    refresh_token = fields.String()
+    access_token = fields.String()
+
+
 class SignInSchema(ma.Schema):
     username = fields.String(required=True)
     password = fields.String(required=True)
@@ -26,6 +31,21 @@ class UpdateUserSchema(ma.Schema):
     first_name = fields.String(required=False)
     last_name = fields.String(required=False)
     active_2FA = fields.Boolean(required=False)
+
+
+class UserSchema(ma.Schema):
+    username = fields.String()
+    email = fields.String()
+    first_name = fields.String()
+    last_name = fields.String()
+    active_2FA = fields.Boolean()
+    id = fields.UUID()
+    updated = fields.DateTime()
+    created = fields.DateTime()
+
+
+class UserRoleSchema(ma.Schema):
+    roles = fields.List(fields.String())
 
 
 class ChangePasswordSchema(ma.Schema):
@@ -48,3 +68,40 @@ class UpdateRoleSchema(ma.Schema):
 class AddRoleSchema(ma.Schema):
     username = fields.String(required=True)
     rolename = fields.String(required=True)
+
+
+class RoleSchema(ma.Schema):
+    id = fields.UUID()
+    name = fields.String()
+    default = fields.Boolean()
+    permissions = fields.Integer()
+    updated = fields.DateTime()
+    created = fields.DateTime()
+
+
+class HistorySchema(ma.Schema):
+    ip_addr = fields.String()
+    timestamp = fields.DateTime()
+    user_agent = fields.String()
+
+
+class ChangedPasswordSchema(ma.Schema):
+    msg = fields.String()
+
+
+class ForbiddenSchema(ma.Schema):
+    error = fields.String()
+    message = fields.String()
+
+
+class UnauthorizedSchema(ma.Schema):
+    msg = fields.String()
+
+
+class BadRequestSchema(ma.Schema):
+    error = fields.String()
+    message = fields.String()
+
+
+class ResponseSchema(ma.Schema):
+    msg = fields.String()
