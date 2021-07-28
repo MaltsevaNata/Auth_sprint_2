0a0077b84e997092f8c5a9bf33be57d9d4464bfa
frbgd <kap17597@gmail.com>
2021-07-28 10:52:30 +0300

    получение данных пользователя от гугла

--- auth_service/src/api/v1/google_oauth.py
+++ auth_service/src/api/v1/google_oauth.py
@@ -1,5 +1,6 @@
 import google_auth_oauthlib.flow
-from flask import redirect
+from flask import redirect, session, url_for, request, jsonify
+from googleapiclient.discovery import build
 
 from core import Config
 from .api_bp import bp
@@ -15,10 +16,46 @@ def sign_in_with_google():
             'https://www.googleapis.com/auth/userinfo.profile'
         ])
 
-    flow.redirect_uri = 'http://127.0.0.1:5000'
+    flow.redirect_uri = url_for('api_v1.google_oauth2callback', _external=True)
 
     authorization_url, state = flow.authorization_url(
         access_type='offline',
         include_granted_scopes='true')
 
+    session['state'] = state
+
     return redirect(authorization_url)
+
+
+@bp.route("/sign_in/google_oauth2callback")
+def google_oauth2callback():
+    state = session['state']
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        Config.GOOGLE_CLIENT_SECRET_FILEPATH,
+        scopes=[
+            'openid',
+            'https://www.googleapis.com/auth/userinfo.email',
+            'https://www.googleapis.com/auth/userinfo.profile'
+        ],
+        state=state)
+
+    flow.redirect_uri = url_for('api_v1.google_oauth2callback', _external=True)
+
+    authorization_response = request.url.replace('http://', 'https://')     # FIXME dev костыль
+    flow.fetch_token(authorization_response=authorization_response)
+
+    credentials = flow.credentials
+    session['credentials'] = {'token': credentials.token,
+                              'refresh_token': credentials.refresh_token,
+                              'token_uri': credentials.token_uri,
+                              'client_id': credentials.client_id,
+                              'client_secret': credentials.client_secret,
+                              'scopes': credentials.scopes}
+
+    service = build('people', 'v1', credentials=credentials)
+    result = service.people().get(
+        resourceName='people/me',
+        personFields='names,emailAddresses'
+    ).execute()
+
+    return jsonify({})
