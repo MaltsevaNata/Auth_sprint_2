b01b14f2bffef8f8470b909572d46e2e8d1f128d
Natalia <maltseva@arrival.com>
2021-07-29 14:53:02 +0300

    Added middleware for authorization

--- api_service/src/main.py
+++ api_service/src/main.py
@@ -1,9 +1,12 @@
 import logging
+import os
+
+import requests
 
 import aioredis
 import uvicorn
 from elasticsearch import AsyncElasticsearch
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 from fastapi.responses import ORJSONResponse
 
 from api.v1 import filmwork, genre, person
@@ -38,6 +41,24 @@ async def shutdown():
 app.include_router(filmwork.router, prefix="/api/v1/film", tags=["film"])
 app.include_router(genre.router, prefix="/api/v1/genre", tags=["genre"])
 app.include_router(person.router, prefix="/api/v1/person", tags=["person"])
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    headers = request.headers
+    if not "authorization" in headers:
+        # здесь нужно возвращать ответ для анонимного пользователя
+        return Response(content="Anonymous user", status_code=401)
+    base_url = "http://" + config.AUTH_URL
+    auth_answer = requests.get(url=f"{base_url}/auth/v1/authorize", headers=headers)
+    if auth_answer.status_code == 200:
+        data = auth_answer.json()
+        if data["roles"]:  # здесь должна быть проверка роли, если просто юзер, то одни данные, если подписчик - другие
+            response = await call_next(request)
+            return response
+        return Response(content="Not allowed", status_code=403)
+    return Response(content="Anonymous user", status_code=401)
+
 
 if __name__ == "__main__":
     uvicorn.run(
