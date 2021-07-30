c1c416d9e12f07f31525b26296ec795768c06a99
frbgd <kap17597@gmail.com>
2021-07-31 01:41:21 +0300

    user_device_type

--- auth_service/src/models/history.py
+++ auth_service/src/models/history.py
@@ -1,5 +1,7 @@
 import datetime
 
+from user_agents import parse
+
 from core import db
 from sqlalchemy.dialects.postgresql import UUID
 
@@ -19,13 +21,30 @@ class LoginHistory(ModelBase):
     user_agent = db.Column(db.String, nullable=False)
     ip_addr = db.Column(db.String, nullable=True)
     timestamp = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
+    user_device_type = db.Column(db.Text, nullable=False, primary_key=True)
 
     def __repr__(self):
         return f'<User {self.user_id} logged in {self.timestamp}>'
+
+    @property
+    def user_device(self):
+        return self.user_device_type
+
+    @user_device.setter
+    def user_device(self, ua_string):
+        user_agent = parse(ua_string)
+        if user_agent.is_mobile:
+            self.user_device_type = 'mobile'
+        elif not user_agent.is_pc and \
+                'smart' in str(user_agent.device.model).lower() or 'smart-tv' in ua_string.lower():
+            self.user_device_type = 'smart'
+        else:
+            self.user_device_type = 'web'
 
     def to_dict(self):
         return {
             'user_agent': self.user_agent,
             'timestamp': dump_datetime(self.timestamp),
-            'ip_addr': self.ip_addr
+            'ip_addr': self.ip_addr,
+            'user_device': self.user_device
         }
